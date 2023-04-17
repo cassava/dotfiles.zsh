1a3a424ff7fec636ea138de18045eced7cf6d6ff
Morgan Benjamin (XC-DX/ETV2) <Benjamin.Morgan@de.bosch.com>
2023-04-17 14:06:32 +0200

    feat: Move CARGO_HOME to .local/share/cargo

--- _profile.d/50-rust.sh
+++ _profile.d/50-rust.sh
@@ -1,4 +1,4 @@
-export CARGO_HOME="$HOME/.cache/cargo"
+export CARGO_HOME="$HOME/.local/share/cargo"
 export CARGO_INSTALL_ROOT="$HOME/.local"
 if command -v rustc >/dev/null; then
     export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
